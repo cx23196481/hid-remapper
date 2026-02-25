3174fbe8ed169d42eb2d90797a8d601aefcbe220
cx23196481 <117374238+cx23196481@users.noreply.github.com>
2026-02-25 16:18:55 +0800

    Add XInput VID and PID definitions

--- firmware/src/xinput_device.h
+++ firmware/src/xinput_device.h
@@ -1,5 +1,10 @@
 #pragma once
 #include <stdint.h>
 
+#define XINPUT_VID 0x045E
+#define XINPUT_PID 0x028E
+
 extern const uint8_t our_report_descriptor_xinput[];
+extern const uint32_t our_report_descriptor_xinput_length;
+
 void xinput_clear_report(uint8_t* report, uint8_t report_id, uint16_t len);
