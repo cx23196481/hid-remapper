95e51a41359d73b9da959b0a1134102e2d4a4239
cx23196481 <117374238+cx23196481@users.noreply.github.com>
2026-02-25 16:22:39 +0800

    Add declaration for our_report_descriptor_xinput_length

--- firmware/src/xinput_device.h
+++ firmware/src/xinput_device.h
@@ -5,6 +5,6 @@
 #define XINPUT_PID 0x028E
 
 extern const uint8_t our_report_descriptor_xinput[];
-extern const uint32_t our_report_descriptor_xinput_length;
+extern const uint32_t our_report_descriptor_xinput_length;  // 加这行
 
 void xinput_clear_report(uint8_t* report, uint8_t report_id, uint16_t len);
